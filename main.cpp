9f2d346a805d0d7416b9efa822a62dfdb8e476f2
cesarion_pshebytski <yur6996@ukr.net>
2018-03-26 21:46:50 +0300

    add map

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <map>
 
 inline std::chrono::high_resolution_clock::time_point get_current_time_fenced() {
     std::atomic_thread_fence(std::memory_order_seq_cst);
@@ -18,75 +19,107 @@ inline long long to_us(const D &d) {
     return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
 }
 
-bool fileExists(const char *path, char *&buffer, std::vector<char *> &lines) {
+int file_to_lines(const char *path, std::vector<char *> &lines) {
+    /// variables declaration
     FILE *pFile;
     long lSize;
     size_t result;
+    char *buffer;
 
+    /// open file from path for reading;
     pFile = fopen(path, "r");
     if (pFile == nullptr) {
         fputs("File error", stderr);
-        return false;
+        return 0;
     }
 
-    // obtain file size:
+    /// obtain file size:
     fseek(pFile, 0, SEEK_END);
     lSize = ftell(pFile);
     rewind(pFile);
 
-    // allocate memory to contain the whole file:
+    /// allocate memory to contain the whole file:
     buffer = (char *) malloc(sizeof(char) * lSize);
     if (buffer == nullptr) {
         fputs("Memory error", stderr);
-        return false;
+        return 0;
     }
 
-    // copy the file into the buffer:
+    /// copy the file into the buffer:
     result = fread(buffer, 1, lSize, pFile);
     if (result != lSize) {
         fputs("Reading error", stderr);
-        return false;
+        return 0;
     }
 
-    /* the whole file is now loaded in the memory buffer. */
-
-    // terminate
-//    std::cout << buffer;
+    /// split buffer into lines separated by whitespace:
     buffer = strtok(buffer, " ");
-    while(buffer){
+    while (buffer) {
         lines.emplace_back(buffer);
         buffer = strtok(nullptr, " ");
     }
-    return fclose(pFile) != 0;
+
+    /// free buffer and return true if everything ok;
+    free(buffer);
+    fclose(pFile);
+    return 1;
 }
 
-void xxx(const char *path, std::vector<std::string> &lines){
-    std::string sLine;
-    std::ifstream infile;
-    infile.open(path);
-
-    while (!infile.eof())
-    {
-        getline(infile, sLine);
-        lines.push_back(sLine);
+void print_if_present(std::map<char *, int> &m, char *el, int &i) {
+    // Звертання m[...] заборонене!
+    // Воно може модифікувати відображення.
+//    auto itr = m.find(el);
+    bool add = true;
+    for (auto &it : m) {
+        if(!strcmp(it.first, el)){
+            add = false;
+            break;
+        }
     }
-    infile.close();
+    if (!add) {
+        auto itr = m.find(el);
+        m[el]+=2;
+        for (auto &it : m) {
+            std::cout << it.first << " : " << it.second << std::endl;
+        }
+        std::cout<< "\n";
+        std::cout << "Element " << el << " is present" << std::endl << "\n";
+    } else {
+        m[el]=0;
+        for (auto &it : m) {
+            std::cout << it.first << " : " << it.second << std::endl;
+        }
+        std::cout<< "\n";
+//        auto it = m.begin();
+//        m.insert(it, std::pair<char *, int>(el, ++i));
+//        std::cout << "Element " << el << " is not present" << std::endl;
+    }
+}
+
+int lines_to_dictionary(const std::vector<char *> lines, std::map<char *, int> &m) {
+    int i = 0;
+    for (char *line: lines) {
+        line = strtok(line, "\n");
+        while (line) {
+            print_if_present(m, line, i);
+            line = strtok(nullptr, " \n");
+        }
+    }
+    return 1;
 }
 
 int main() {
-    char *buffer;
-//    std::vector<std::string> lines;
-    std::vector<char *> arr;
+    std::vector<char *> lines;
     const char *path = "/Users/cesarion_pshebytski/Downloads/DataSource/Loyko_Aeroport_RuLit_Me_421937.txt";
-//    auto stage1_start_time = get_current_time_fenced();
-//    xxx(path, lines);
-//    std::cout << "\nTime : " << to_us(get_current_time_fenced() - stage1_start_time) << std::endl;
-//    std::cout<<"\n"<<lines.size();
-//    ////
-//    lines.clear();
-    fileExists(path, buffer, arr);
-    std::cout<<"\n"<<arr.size();
+    path = "example.txt";
+    std::map<char *, int> m;
+    file_to_lines(path, lines);
+    lines_to_dictionary(lines, m);
+    std::cout << "\n" << lines.size() << "\n";
+    for (auto &it : m) {
+        std::cout << it.first << " : " << it.second << std::endl;
+    }
 
-    free(buffer);
+
     return 0;
 }
